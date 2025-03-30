7458008667e2570189fa8fd8f3c711587424cb00
Arthur <arthur.hupsel@trigodourado.com>
2025-03-30 15:08:28 -0300

    upload files

--- database/db.go
+++ database/db.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"log"
-
+	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,7 +14,10 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+
+	os.Getenv("HOST")
+
+	stringDeConexao := "host="+OS.Getenv("HOST")+" user="+OS.Getenv("USER")+" password="+OS.Getenv("PASSWORD")+" dbname="+OS.Getenv("DBNAME")+" port="+OS.Getenv("PORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
